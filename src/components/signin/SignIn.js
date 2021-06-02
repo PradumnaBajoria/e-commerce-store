97124257ceb97adbbd60efa4162798ec09688357
Pradumna <pradumnabajoria@gmail.com>
2021-06-02 20:58:26 +0530

    started setting up redux to use in application

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -52,7 +52,7 @@ function SignIn() {
                 />
                 <div className='buttons'>
                 <CustomButton type='submit'>Sign In</CustomButton>
-                <CustomButton onClick={signInWithGoogle} isGoogleSignIn >Sign In With Google</CustomButton>
+                <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn >Sign In With Google</CustomButton>
                 </div>
             </form>
 
---
+++ src/redux/Store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+import rootReducer from "./RootReducer"
+
+const middlewares = [logger]
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares))
+//const store = createStore(rootReducer, applyMiddleware(logger))
+
+export default store
