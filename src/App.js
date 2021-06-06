e0d11d31f70c2236325d0b80191a10b4d0b4497f
Pradumna <pradumnabajoria@gmail.com>
2021-06-06 23:45:27 +0530

    added feature to redirect to homepage if user is sign in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
 import './App.css';
 import HomePage from "./pages/homepage/HomePage"
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route, Redirect } from "react-router-dom"
 import Shop from "./pages/shop/Shop"
 import Header from "./components/header/Header"
 import SignInSignUp from "./pages/signin-signup/SignInSignUp"
@@ -46,14 +46,18 @@ function App(props) {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={Shop} />
-        <Route exact path='/signin' component={SignInSignUp} />
+        <Route exact path='/signin' render={() => props.currentUser ? (<Redirect to="/" />) : (<SignInSignUp />)} />
       </Switch>
     </div>
   );
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser : user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
