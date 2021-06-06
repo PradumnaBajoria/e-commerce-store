a4711747b1ff42295b01eea397d49bf052eb5f7f
Pradumna <pradumnabajoria@gmail.com>
2021-06-07 00:49:52 +0530

    added cart ion and dropdown and implemented it's reducer

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom"
 import { ReactComponent as Logo } from "../../assets/crown.svg"
 import { auth } from "../../firebase/Firebase"
 import { connect } from "react-redux"
+import CartIcon from "../cart-icon/CartIcon"
+import CartDropDown from "../cart-dropdown/CartDropDown"
 
 
-function Header({ currentUser }) {
+function Header({ currentUser, hidden }) {
     return (
         <div className="header">
             <Link to="/" className="logo-container" >
@@ -25,13 +27,16 @@ function Header({ currentUser }) {
                     <div className='option' onClick={() => auth.signOut()} >SIGN OUT</div> 
                     : <Link className='option' to='/signin' >SIGN IN</Link>
                 }
+                <CartIcon />
             </div>
+            { hidden ? null : <CartDropDown /> }
         </div>
     )
 }
 
-const mapStateToProps = state => ({
-    currentUser : state.user.currentUser
+const mapStateToProps = ({user : { currentUser }, cart : { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header)
--- src/redux/RootReducer.js
+++ src/redux/RootReducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
 import UserReducer from './user/UserReducer'
-
+import CartReducer from './cart/CartReducer'
 
 export default combineReducers({
-    user: UserReducer
+    user: UserReducer,
+    cart: CartReducer
 })
