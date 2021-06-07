fb0483c77c9a072fb115e4c3b3e93e774b3bf820
Pradumna <pradumnabajoria@gmail.com>
2021-06-07 15:56:47 +0530

    updated shop data(id) and added feature in add to cart button

--- src/components/collection-item/CollectionItem.js
+++ src/components/collection-item/CollectionItem.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import "./CollectionItem.scss"
 import CustomButton from "../custom-button/CustomButton"
+import { connect } from "react-redux"
+import { addItem } from "../../redux/cart/CartAction"
 
-function CollectionItem({ id, name, imageUrl, price }) {
+function CollectionItem({ item, addItem }) {
+
+    const { name, price, imageUrl } = item
+
     return (
         <div className="collection-item">
             <div
@@ -15,9 +20,13 @@ function CollectionItem({ id, name, imageUrl, price }) {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <CustomButton inverted> ADD TO CART </CustomButton>
+            <CustomButton onClick={() => addItem(item)} inverted> ADD TO CART </CustomButton>
         </div>
     )
 }
 
-export default CollectionItem
+const mapDispatchToProps = dispatch => ({
+    addItem : item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem)
